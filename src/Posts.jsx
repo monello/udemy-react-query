11345a7a0e3d98e19338443620dc96a6a847531e
Morné Louw <morne.louw@stepstone.com>
2023-01-28 09:16:06 +0200

    feat: basic example of useQuery

--- src/Posts.jsx
+++ src/Posts.jsx
@@ -1,46 +1,52 @@
 import { useState } from "react";
+import { useQuery } from "react-query";
 
 import { PostDetail } from "./PostDetail";
 const maxPostPage = 10;
 
 async function fetchPosts() {
-  const response = await fetch(
-    "https://jsonplaceholder.typicode.com/posts?_limit=10&_page=0"
-  );
-  return response.json();
+    const response = await fetch(
+        "https://jsonplaceholder.typicode.com/posts?_limit=10&_page=0"
+    );
+    return response.json();
 }
 
 export function Posts() {
-  const [currentPage, setCurrentPage] = useState(0);
-  const [selectedPost, setSelectedPost] = useState(null);
+    const [currentPage, setCurrentPage] = useState(0);
+    const [selectedPost, setSelectedPost] = useState(null);
 
-  // replace with useQuery
-  const data = [];
+    // Destructure the "data" ptoperty that gets returned from the useQuery() hook
+    // There are many other properties that can be destructured.
+    // See: https://react-query-v3.tanstack.com/guides/queries and https://react-query-v3.tanstack.com/reference/useQuery
+    const { data } = useQuery("posts", fetchPosts);
 
-  return (
-    <>
-      <ul>
-        {data.map((post) => (
-          <li
-            key={post.id}
-            className="post-title"
-            onClick={() => setSelectedPost(post)}
-          >
-            {post.title}
-          </li>
-        ))}
-      </ul>
-      <div className="pages">
-        <button disabled onClick={() => {}}>
-          Previous page
-        </button>
-        <span>Page {currentPage + 1}</span>
-        <button disabled onClick={() => {}}>
-          Next page
-        </button>
-      </div>
-      <hr />
-      {selectedPost && <PostDetail post={selectedPost} />}
-    </>
-  );
+    // temoporary hack to make component re-render once the data returns from the fetchPosts (Async call)
+    if (!data) return <div />;
+
+    return (
+        <>
+            <ul>
+                {data.map((post) => (
+                    <li
+                        key={post.id}
+                        className="post-title"
+                        onClick={() => setSelectedPost(post)}
+                    >
+                        {post.title}
+                    </li>
+                ))}
+            </ul>
+            <div className="pages">
+                <button disabled onClick={() => {}}>
+                    Previous page
+                </button>
+                <span>Page {currentPage + 1}</span>
+                <button disabled onClick={() => {}}>
+                    Next page
+                </button>
+            </div>
+            <hr />
+            {selectedPost && <PostDetail post={selectedPost} />}
+        </>
+    );
 }
