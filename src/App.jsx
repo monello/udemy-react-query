31c8e0f7f38c64d817acf88b128d52eae5014397
Morné Louw <morne.louw@stepstone.com>
2023-01-28 08:29:41 +0200

    chore: create and provide the query-provider to the application

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
+import { QueryClient, QueryClientProvider } from "react-query";
+
 import { Posts } from "./Posts";
 import "./App.css";
 
+const queryClient = new QueryClient();
+
 function App() {
-  return (
-    // provide React Query client to App
-    <div className="App">
-      <h1>Blog Posts</h1>
-      <Posts />
-    </div>
-  );
+    return (
+        // provide React Query client to App
+        <QueryClientProvider client={queryClient}>
+            <div className="App">
+                <h1>Blog Posts</h1>
+                <Posts />
+            </div>
+        </QueryClientProvider>
+    );
 }
 
 export default App;
