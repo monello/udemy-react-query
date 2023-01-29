b7121da1c1ca26b97e3c2f94567036d24f93a0b9
Morné Louw <louw.morne@gmail.com>
2023-01-29 17:12:00 +0200

    feat: s2v14 - ading basic pagination

--- src/Posts.jsx
+++ src/Posts.jsx
@@ -4,21 +4,28 @@ import { useQuery } from "react-query";
 import { PostDetail } from "./PostDetail";
 const maxPostPage = 10;
 
-async function fetchPosts() {
+async function fetchPosts(pageNumber) {
     const response = await fetch(
-        "https://jsonplaceholder.typicode.com/posts?_limit=10&_page=0"
+        `https://jsonplaceholder.typicode.com/posts?_limit=10&_page=${pageNumber}`
     );
     return response.json();
 }
 
 export function Posts() {
-    const [currentPage, setCurrentPage] = useState(0);
+    const [currentPage, setCurrentPage] = useState(1);
     const [selectedPost, setSelectedPost] = useState(null);
 
     // Destructure the "data" ptoperty that gets returned from the useQuery() hook
     // There are many other properties that can be destructured.
     // See: https://react-query-v3.tanstack.com/guides/queries and https://react-query-v3.tanstack.com/reference/useQuery
-    const { data, isLoading, isError, error } = useQuery("posts", fetchPosts);
+    const { data, isLoading, isError, error } = useQuery(
+        // Set a unique query-key for each page
+        ["posts", currentPage],
+        () => fetchPosts(currentPage),
+        {
+            staleTime: 2000,
+        }
+    );
 
     if (isLoading) return <div>Loading ...</div>;
     if (isError)
@@ -43,11 +50,24 @@ export function Posts() {
                 ))}
             </ul>
             <div className="pages">
-                <button disabled onClick={() => {}}>
+                <button
+                    disabled={currentPage <= 1}
+                    onClick={() => {
+                        // When setting a new state value that must be based on the previous value, use the Previous-pattern
+                        // https://beta.reactjs.org/reference/react/useState#setstate
+                        // https://beta.reactjs.org/reference/react/useState#updating-state-based-on-the-previous-state
+                        setCurrentPage((previousValue) => previousValue - 1);
+                    }}
+                >
                     Previous page
                 </button>
-                <span>Page {currentPage + 1}</span>
-                <button disabled onClick={() => {}}>
+                <span>Page {currentPage}</span>
+                <button
+                    disabled={currentPage >= maxPostPage}
+                    onClick={() => {
+                        setCurrentPage((previousValue) => previousValue + 1);
+                    }}
+                >
                     Next page
                 </button>
             </div>
